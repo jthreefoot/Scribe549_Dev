327344fddfd1cb3a3bcbc0ad6bd43eac5b124ccb
Jane Threefoot <jane.threefoot@gmail.com>
2016-02-21 22:46:27 -0500

    now it saves the coords to a txt file, also noting the start/end of a stroke

--- drawingTestData/gpServer.js
+++ drawingTestData/gpServer.js
@@ -2,15 +2,19 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
+var fs = require('fs');
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname+'/getPoints.html'));
 });
 
 io.on('connection', function(socket) {
-    process.stdout.write('a user connected\n');
+    console.log('a user connected\n');
     socket.on('client_data', function(data) {
-	process.stdout.write(data.coord + '\n');
+	//console.log(data.coord);
+	fs.appendFile('tmp.txt', data.coord+'\n', function(err) {
+	    if (err) return console.log(err);
+	});
     });
 });
 
