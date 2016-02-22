a00537f3276a219b88444a556adab227dbf9debd
Jane Threefoot <jane.threefoot@gmail.com>
2016-02-21 23:01:56 -0500

    moved client code to separate file

--- drawingTestData/gpServer.js
+++ drawingTestData/gpServer.js
@@ -1,8 +1,11 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
 var fs = require('fs');
+
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname+'/getPoints.html'));
---
+++ drawingTestData/public/gpClient.js
@@ -0,0 +1,42 @@
+
+var socket = io();
+
+var c = document.getElementById("can");
+var ctx = c.getContext("2d");
+c.addEventListener("mousemove", getCurrCoord, false);
+c.addEventListener("mousedown", getMD, false);
+c.addEventListener("mouseup", getMU, false);
+
+var drawing = false;
+
+function getCurrCoord(event) {
+    if (drawing == true) {
+        var c_x = event.clientX;
+        var c_y = event.clientY;
+        ctx.font = "15pt Arial";
+        var msg = "(" + c_x + "," + c_y + ")";
+        ctx.clearRect(0, 0, 400, 400);
+        ctx.fillText(msg, 20, 20);
+        socket.emit("client_data", {"coord": msg});
+    }
+}
+
+function getCoord(event, dir) {
+    var c_x = event.clientX;
+    var c_y = event.clientY;
+    return dir + " at: " + c_x + "," + c_y;
+}
+
+function getMD(event) {
+    drawing = true;
+    var msg = getCoord(event, "down");
+    document.getElementById("down").innerHTML = msg;
+    socket.emit("client_data", {"coord": "start"});
+}
+
+function getMU(event) {
+    drawing = false;
+    var msg = getCoord(event, "up");
+    document.getElementById("up").innerHTML = msg;
+    socket.emit("client_data", {"coord": "end"});
+}
