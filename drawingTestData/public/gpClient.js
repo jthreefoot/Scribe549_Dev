c443903ceda9a91b32b5588498158a1c0d676e0d
Jane Threefoot <jane.threefoot@gmail.com>
2016-02-21 23:45:20 -0500

    draws w proper offsets and scaling now

--- drawingTestData/public/gpClient.js
+++ drawingTestData/public/gpClient.js
@@ -1,7 +1,8 @@
-
 var socket = io();
 
 var c = document.getElementById("can");
+var can = $("#can");
+var offset = can.offset();
 var ctx = c.getContext("2d");
 c.addEventListener("mousemove", getCurrCoord, false);
 c.addEventListener("mousedown", getMD, false);
@@ -9,21 +10,26 @@ c.addEventListener("mouseup", getMU, false);
 
 var drawing = false;
 
+
 function getCurrCoord(event) {
     if (drawing == true) {
-        var c_x = event.clientX;
-        var c_y = event.clientY;
-        ctx.font = "15pt Arial";
+        var c_x = event.clientX - offset.left;
+        var c_y = event.clientY - offset.top;
+        //ctx.font = "15pt Arial";
         var msg = "(" + c_x + "," + c_y + ")";
-        ctx.clearRect(0, 0, 400, 400);
-        ctx.fillText(msg, 20, 20);
+        //ctx.clearRect(0, 0, 400, 400);
+        //ctx.fillText(msg, 20, 20);
         socket.emit("client_data", {"coord": msg});
+	
+	//draw
+	ctx.lineTo(c_x,c_y);
+	ctx.stroke();
     }
 }
 
 function getCoord(event, dir) {
-    var c_x = event.clientX;
-    var c_y = event.clientY;
+    var c_x = event.clientX - offset.left;
+    var c_y = event.clientY - offset.top;
     //return dir + " at: " + c_x + "," + c_y;
     return "(" + c_x + "," + c_y + ")";
 }
@@ -35,6 +41,11 @@ function getMD(event) {
     socket.emit("client_data", {"coord": "start"});
     // make sure to record first point where user clicks
     socket.emit("client_data", {"coord": msg});
+    //draw
+    ctx.beginPath();
+    var c_x = event.clientX - offset.left;
+    var c_y = event.clientY - offset.top;
+    ctx.moveTo(c_x, c_y);
 }
 
 function getMU(event) {
@@ -42,4 +53,9 @@ function getMU(event) {
     var msg = getCoord(event, "up");
     document.getElementById("up").innerHTML = msg;
     socket.emit("client_data", {"coord": "end"});
+    //draw
+    var x = event.clientX - offset.left;
+    var y = event.clientY - offset.top;
+    ctx.lineTo(x,y);
+    ctx.stroke();
 }
