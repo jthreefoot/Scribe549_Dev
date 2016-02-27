db72411088a1d7e59a38042b738cf9aa0beb043e
Jane Threefoot <jane.threefoot@gmail.com>
2016-02-27 16:50:26 -0500

    can name datafiles from clientside, plus a little housekeeping

--- drawingTestData/gpServer.js
+++ drawingTestData/gpServer.js
@@ -4,10 +4,22 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
 var fs = require('fs');
+var bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var datadir = 'datafiles/';
+var filename = 'tmp';
 
 app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname+'/nameTest.html'));
+});
+
+app.post('/name', function(req, res) {
+    filename = req.body.filename;
+    //console.log(filename);
     res.sendFile(path.join(__dirname+'/getPoints.html'));
 });
 
@@ -15,7 +27,8 @@ io.on('connection', function(socket) {
     console.log('a user connected\n');
     socket.on('client_data', function(data) {
 	//console.log(data.coord);
-	fs.appendFile('tmp.txt', data.coord+'\n', function(err) {
+	var txtname = datadir + filename + '.txt';
+	fs.appendFile(txtname, data.coord+'\n', function(err) {
 	    if (err) return console.log(err);
 	});
     });
